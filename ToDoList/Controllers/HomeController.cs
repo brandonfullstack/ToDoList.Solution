33fcf181cc726170de6c6c5889eed177f27f0f4d
Brandon Wright <brandonfullstack@gmail.com>
2023-09-13 23:25:12 -0400

    Add multiple controllers

--- ToDoList/Controllers/HomeController.cs
+++ ToDoList/Controllers/HomeController.cs
@@ -1,29 +1,15 @@
 using Microsoft.AspNetCore.Mvc;
-using ToDoList.Models;
 
 namespace ToDoList.Controllers
 {
-  public class HomeController : Controller
-  {
+    public class HomeController : Controller
+    {
 
-    [HttpGet("/")]
-    public ActionResult Index()
-    {
-      Item starterItem = new Item("Add first item to To Do List");
-      return View(starterItem);
-    }
+      [HttpGet("/")]
+      public ActionResult Index()
+      {
+        return View();
+      }
 
-    [HttpGet("/items/new")]
-    public ActionResult CreateForm()
-    {
-      return View();
     }
-    
-    [HttpPost("/items")]
-    public ActionResult Create(string description)
-    {
-      Item myItem = new Item(description);
-      return View("Index", myItem);
-    }
-  }
 }
