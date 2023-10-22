f031a516c7b04c94c1f58ac74d6fa485d58a47f5
Brandon Wright <brandonfullstack@gmail.com>
2023-10-22 16:18:58 -0400

    WIP: Add splash page

--- ToDoList/Controllers/ItemsController.cs
+++ ToDoList/Controllers/ItemsController.cs
@@ -33,13 +33,17 @@ namespace ToDoList.Controllers
     [HttpPost]
     public ActionResult Create(Item item)
     {
-      if (item.CategoryId == 0)
+      if (!ModelState.IsValid)
       {
-        return RedirectToAction("Create");
+          ViewBag.CategoryId = new SelectList(_db.Categories, "CategoryId", "Name");
+          return View(item);
       }
-      _db.Items.Add(item);
-      _db.SaveChanges();
-      return RedirectToAction("Index");
+      else
+      {
+        _db.Items.Add(item);
+        _db.SaveChanges();
+        return RedirectToAction("Index");
+      }
     }
 
     public ActionResult Details(int id)
@@ -101,6 +105,15 @@ namespace ToDoList.Controllers
         _db.SaveChanges();
       }
       return RedirectToAction("Details", new { id = item.ItemId });
-    }  
+    }
+
+    [HttpPost]
+    public ActionResult DeleteJoin(int joinId)
+    {
+      ItemTag joinEntry = _db.ItemTags.FirstOrDefault(entry => entry.ItemTagId == joinId);
+      _db.ItemTags.Remove(joinEntry);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
   }
 }
