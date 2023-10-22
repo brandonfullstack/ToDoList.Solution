bc07692fec3c1c55864b74c9b8c27428deb2c08b
Brandon Wright <brandonfullstack@gmail.com>
2023-10-22 19:08:12 -0400

    Add authentication and authorization

--- ToDoList/Program.cs
+++ ToDoList/Program.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using ToDoList.Models;
+using Microsoft.AspNetCore.Identity;
 
 namespace ToDoList
 {
@@ -9,6 +10,7 @@ namespace ToDoList
   {
     static void Main(string[] args)
     {
+
       WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
       builder.Services.AddControllersWithViews();
@@ -21,19 +23,45 @@ namespace ToDoList
                         )
                       );
 
+      builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
+                .AddEntityFrameworkStores<ToDoListContext>()
+                .AddDefaultTokenProviders();
+
+      builder.Services.Configure<IdentityOptions>(options =>
+      {
+        // Default Password settings.
+        // options.Password.RequireDigit = true;
+        // options.Password.RequireLowercase = true;
+        // options.Password.RequireNonAlphanumeric = true;
+        // options.Password.RequireUppercase = true;
+        // options.Password.RequiredLength = 6;
+        // options.Password.RequiredUniqueChars = 1;
+
+        options.Password.RequireDigit = false;
+        options.Password.RequireLowercase = false;
+        options.Password.RequireNonAlphanumeric = false;
+        options.Password.RequireUppercase = false;
+        options.Password.RequiredLength = 0;
+        options.Password.RequiredUniqueChars = 0;
+      });
+
       WebApplication app = builder.Build();
 
-      // app.UseDeveloperExceptionPage();
-      app.UseHttpsRedirection();
+    // app.UseDeveloperExceptionPage();
+    app.UseHttpsRedirection();
       app.UseStaticFiles();
 
       app.UseRouting();
 
+      app.UseAuthentication();
+      app.UseAuthorization();
+
       app.MapControllerRoute(
           name: "default",
-          pattern: "{controller=Home}/{action=Index}/{id?}");
+          pattern: "{controller=Home}/{action=Index}/{id?}"
+        );
 
       app.Run();
     }
-  }
+}
 }
